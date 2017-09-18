fec4c2d0cf5a4c012e484fb09828af0e5cd4a43b
Ralf Boehm <ralf.boehm@cape-it.de>
2017-09-18 09:52:26 +0200

    implemented Priority Operations

---
+++ Kernel/API/Operation/V1/Priority/PrioritySearch.pm
@@ -0,0 +1,144 @@
+# --
+# Kernel/API/Operation/Priority/PriorityCreate.pm - API Priority Create operation backend
+# Copyright (C) 2006-2016 c.a.p.e. IT GmbH, http://www.cape-it.de
+#
+# written/edited by:
+# * Rene(dot)Boehm(at)cape(dash)it(dot)de
+# 
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::API::Operation::V1::Priority::PrioritySearch;
+
+use strict;
+use warnings;
+
+use Kernel::API::Operation::V1::Priority::PriorityGet;
+use Kernel::System::VariableCheck qw(:all);
+
+use base qw(
+    Kernel::API::Operation::V1::Common
+);
+
+our $ObjectManagerDisabled = 1;
+
+=head1 NAME
+
+Kernel::API::Operation::V1::Priority::PrioritySearch - API Priority Search Operation backend
+
+=head1 PUBLIC INTERFACE
+
+=over 4
+
+=cut
+
+=item new()
+
+usually, you want to create an instance of this
+by using Kernel::API::Operation->new();
+
+=cut
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    my $Self = {};
+    bless( $Self, $Type );
+
+    # check needed objects
+    for my $Needed (qw(DebuggerObject WebserviceID)) {
+        if ( !$Param{$Needed} ) {
+            return $Self->_Error(
+                Code    => 'Operation.InternalError',
+                Message => "Got no $Needed!"
+            );
+        }
+
+        $Self->{$Needed} = $Param{$Needed};
+    }
+
+    return $Self;
+}
+
+=item Run()
+
+perform PrioritySearch Operation. This will return a Priority ID list.
+
+    my $Result = $OperationObject->Run(
+        Data => {
+        }
+    );
+
+    $Result = {
+        Success => 1,                           # 0 or 1
+        Message => '',                               # In case of an error
+        Data    => {
+            PriorityID => [ 1, 2, 3, 4 ],
+        },
+    };
+
+=cut
+
+sub Run {
+    my ( $Self, %Param ) = @_;
+
+    my $Result = $Self->Init(
+        WebserviceID => $Self->{WebserviceID},
+    );
+
+    if ( !$Result->{Success} ) {
+        $Self->_Error(
+            Code    => 'Webservice.InvalidConfiguration',
+            Message => $Result->{Message},
+        );
+    }
+
+    # prepare data
+    $Result = $Self->PrepareData(
+        Data       => $Param{Data},
+    );
+
+    # check result
+    if ( !$Result->{Success} ) {
+        return $Self->_Error(
+            Code    => 'Operation.PrepareDataError',
+            Message => $Result->{Message},
+        );
+    }
+
+    # perform Priority search
+    my %PriorityList = $Kernel::OM->Get('Kernel::System::Priority')->PriorityList(
+        Valid => 1,
+    );
+
+    if (IsHashRefWithData(\%PriorityList)) {
+        my $PriorityGetResult = $Self->ExecOperation(
+            OperationType => 'V1::Priority::PriorityGet',
+            Data      => {
+                PriorityID => join(',', sort keys %PriorityList),
+            }
+        );
+ 
+        if ( !IsHashRefWithData($PriorityGetResult) || !$PriorityGetResult->{Success} ) {
+            return $PriorityGetResult;
+        }
+
+        my @PriorityDataList = IsArrayRefWithData($PriorityGetResult->{Data}->{Priority}) ? @{$PriorityGetResult->{Data}->{Priority}} : ( $PriorityGetResult->{Data}->{Priority} );
+
+        if ( IsArrayRefWithData(\@PriorityDataList) ) {
+            return $Self->_Success(
+                Priority => \@PriorityDataList,
+            )
+        }
+    }
+
+    # return result
+    return $Self->_Success(
+        Priority => {},
+    );
+}
+
+1;
---
+++ Kernel/API/Operation/V1/Priority/PriorityUpdate.pm
@@ -0,0 +1,174 @@
+# --
+# Kernel/API/Operation/Priority/PriorityUpdate.pm - API Priority Update operation backend
+# Copyright (C) 2006-2016 c.a.p.e. IT GmbH, http://www.cape-it.de
+#
+# written/edited by:
+# * Rene(dot)Boehm(at)cape(dash)it(dot)de
+# 
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::API::Operation::V1::Priority::PriorityUpdate;
+
+use strict;
+use warnings;
+
+use Kernel::System::VariableCheck qw(IsArrayRefWithData IsHashRefWithData IsStringWithData);
+
+use base qw(
+    Kernel::API::Operation::V1::Common
+);
+
+our $ObjectManagerDisabled = 1;
+
+=head1 NAME
+
+Kernel::API::Operation::V1::Priority::PriorityUpdate - API Priority Create Operation backend
+
+=head1 SYNOPSIS
+
+=head1 PUBLIC INTERFACE
+
+=over 4
+
+=cut
+
+=item new()
+
+usually, you want to create an instance of this
+by using Kernel::API::Operation->new();
+
+=cut
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    my $Self = {};
+    bless( $Self, $Type );
+
+    # check needed objects
+    for my $Needed (qw( DebuggerObject WebserviceID )) {
+        if ( !$Param{$Needed} ) {
+            return $Self->_Error(
+                Code    => 'Operation.InternalError',
+                Message => "Got no $Needed!"
+            );
+        }
+
+        $Self->{$Needed} = $Param{$Needed};
+    }
+
+    $Self->{Config} = $Kernel::OM->Get('Kernel::Config')->Get('API::Operation::V1::PriorityUpdate');
+
+    return $Self;
+}
+
+=item Run()
+
+perform PriorityUpdate Operation. This will return the updated TypeID.
+
+    my $Result = $OperationObject->Run(
+        Data => {
+            ID      => '...',
+        }
+	    Priority => {
+	        Name    => '...',
+	        ValidID => '...',
+	    },
+	);
+    
+
+    $Result = {
+        Success     => 1,                       # 0 or 1
+        Message     => '',                      # in case of error
+        Data        => {                        # result data payload after Operation
+            PriorityID  => '',                  # PriorityID 
+            Error   => {                        # should not return errors
+                    Code    => 'Priority.Update.ErrorCode'
+                    Message => 'Error Description'
+            },
+        },
+    };
+   
+=cut
+
+
+sub Run {
+    my ( $Self, %Param ) = @_;
+
+    # init webservice
+    my $Result = $Self->Init(
+        WebserviceID => $Self->{WebserviceID},
+    );
+
+    if ( !$Result->{Success} ) {
+        $Self->_Error(
+            Code    => 'Webservice.InvalidConfiguration',
+            Message => $Result->{Message},
+        );
+    }
+use Data::Dumper;
+print STDERR "param".Dumper(\%Param);
+    # prepare data
+    $Result = $Self->PrepareData(
+        Data         => $Param{Data},
+        Parameters   => {
+            'PriorityID' => {
+                Type => 'HASH',
+                Required => 1
+            },
+            'Priority::Priority' => {
+                Required => 1
+            },
+            'Priority::ValidID' => {
+                Required => 1
+            },                    
+        }        
+    );
+
+    # check result
+    if ( !$Result->{Success} ) {
+        return $Self->_Error(
+            Code    => 'Operation.PrepareDataError',
+            Message => $Result->{Message},
+        );
+    }
+
+    # check if Priority exists 
+    my %PriorityData = $Kernel::OM->Get('Kernel::System::Priority')->PriorityGet(
+        PriorityID => $Param{Data}->{PriorityID},
+        UserID  => $Self->{Authorization}->{UserID},
+    );
+    
+    if ( !%PriorityData ) {
+        return $Self->_Error(
+            Code    => 'Object.NotFound',
+            Message => "Can not patch Priority. Priority with this ID '$Param{Data}->{PriorityID}' not exists.",
+        );
+    }
+
+    # update Priority
+    my $Success = $Kernel::OM->Get('Kernel::System::Priority')->PriorityUpdate(
+        PriorityID      => $Param{Data}->{PriorityID},
+        Name    => $Param{Data}->{Priority}->{Priority},
+        ValidID => $Param{Data}->{Priority}->{ValidID},
+        UserID  => $Self->{Authorization}->{UserID},
+    );
+
+    if ( !$Success ) {
+        return $Self->_Error(
+            Code    => 'Object.UnableToUpdate',
+            Message => 'Could not update Priority, please contact the system administrator',
+        );
+    }
+
+    # return result    
+    return $Self->_Success(
+        TypeID => $PriorityData{ID},
+    );    
+}
+
+
