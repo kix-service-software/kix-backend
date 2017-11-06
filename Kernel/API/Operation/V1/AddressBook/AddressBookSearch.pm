629490e70aa491dc5e1764a21904328f8b9e8246
René Böhm <rene.boehm@cape-it.de>
2017-11-06 13:18:09 +0100

    fixed typo

--- Kernel/API/Operation/V1/AddressBook/AddressBookSearch.pm
+++ Kernel/API/Operation/V1/AddressBook/AddressBookSearch.pm
@@ -95,7 +95,7 @@ sub Run {
 
     if ( !$Result->{Success} ) {
         $Self->_Error(
-            Code    => 'Webservice.InInvalidConfiguration',
+            Code    => 'Webservice.InvalidConfiguration',
             Message => $Result->{Message},
         );
     }
