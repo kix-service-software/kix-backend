6fbe3ae4f5f2d81c1624d0eb86a9af843ab6d37e
ralf <ralf@herakles>
2018-03-07 08:34:11 +0100

    vorbereitet

---
+++ Kernel/API/Operation/V1/Ticket/ArticleFlagGet.pm
@@ -0,0 +1,218 @@
+# --
+# Modified version of the work: Copyright (C) 2006-2017 c.a.p.e. IT GmbH, http://www.cape-it.de
+# based on the original work of:
+# Copyright (C) 2001-2017 OTRS AG, http://otrs.com/
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::API::Operation::V1::Ticket::ArticleFlagGet;
+
+use strict;
+use warnings;
+
+use MIME::Base64;
+
+use Kernel::System::VariableCheck qw(IsArrayRefWithData IsHashRefWithData IsStringWithData);
+
+use base qw(
+    Kernel::API::Operation::V1::Ticket::Common
+);
+
+our $ObjectManagerDisabled = 1;
+
+=head1 NAME
+
+Kernel::API::Operation::V1::Ticket::ArticleFlagGet - API Ticket Get Operation backend
+
+=head1 SYNOPSIS
+
+=head1 PUBLIC INTERFACE
+
+=over 4
+
+=cut
+
+=item new()
+
+usually, you want to create an instance of this
+by using Kernel::API::Operation::V1->new();
+
+=cut
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    my $Self = {};
+    bless( $Self, $Type );
+
+    # check needed objects
+    for my $Needed (qw(DebuggerObject WebserviceID)) {
+        if ( !$Param{$Needed} ) {
+            return {
+                Success      => 0,
+                ErrorMessage => "Got no $Needed!",
+            };
+        }
+
+        $Self->{$Needed} = $Param{$Needed};
+    }
+
+    return $Self;
+}
+
+=item Run()
+
+perform ArticleFlagGet Operation. This function is able to return
+one or more ticket entries in one call.
+
+    my $Result = $OperationObject->Run(
+        Data => {
+            TicketID             => '1',                                           # required 
+            ArticleID            => '32',                                          # required, could be coma separated IDs or an Array
+            AttachmentID         => ':1',                                          # required, could be coma separated IDs or an Array
+            include              => '...',                                         # Optional, 0 as default. Include additional objects
+                                                                                   # (supported: Content)
+        },
+    );
+
+    $Result = {
+        Success      => 1,                                # 0 or 1
+        Code         => '',                               # In case of an error
+        Message      => '',                               # In case of an error
+        Data         => {
+            Attachment => [
+                {
+                    AttachmentID      => 123
+                    ContentAlternative => "",
+                    ContentID          => "",
+                    ContentType        => "application/pdf",
+                    Filename           => "StdAttachment-Test1.pdf",
+                    Filesize           => "4.6 KBytes",
+                    FilesizeRaw        => 4722,
+                },
+                {
+                    #. . .
+                },
+            ],
+        },
+    };
+
+=cut
+
+sub Run {
+    my ( $Self, %Param ) = @_;
+
+    # init webservice
+    my $Result = $Self->Init(
+        WebserviceID => $Self->{WebserviceID},
+    );
+
+    if ( !$Result->{Success} ) {
+        $Self->_Error(
+            Code    => 'Webservice.InvalidConfiguration',
+            Message => $Result->{Message},
+        );
+    }
+
+    # prepare data
+    $Result = $Self->PrepareData(
+        Data       => $Param{Data},
+        Parameters => {
+            'TicketID' => {
+                Required => 1
+            },
+            'ArticleID' => {
+                Required => 1
+            },
+            'AttachmentID' => {
+                Type     => 'ARRAY',
+                DataType => 'NUMERIC',
+                Required => 1
+            },
+        }
+    );
+
+    # check result
+    if ( !$Result->{Success} ) {
+        return $Self->_Error(
+            Code    => 'Operation.PrepareDataError',
+            Message => $Result->{Message},
+        );
+    }
+
+    # check ticket permission
+    my $Permission = $Self->CheckAccessPermission(
+        TicketID => $Param{Data}->{TicketID},
+        UserID   => $Self->{Authorization}->{UserID},
+        UserType => $Self->{Authorization}->{UserType},
+    );
+
+    if ( !$Permission ) {
+        return $Self->_Error(
+            Code    => 'Object.NoPermission',
+            Message => "No permission to access ticket $Param{Data}->{TicketID}.",
+        );
+    }
+
+    # get ticket object
+    my $TicketObject = $Kernel::OM->Get('Kernel::System::Ticket');
+
+    my @AttachmentList;
+
+    # start attachment loop
+    ATTACHMENT:
+    foreach my $AttachmentID ( sort @{$Param{Data}->{AttachmentID}} ) {
+        
+        my %Attachment = $TicketObject->ArticleFlag(
+            ArticleID          => $Param{Data}->{ArticleID},
+            FileID             => $AttachmentID,
+            UserID             => $Self->{Authorization}->{UserID},
+        );
+
+        # add ID to result
+        $Attachment{ID} = $AttachmentID;
+
+        if ( !$Param{Data}->{include}->{Content} ) {
+            delete $Attachment{Content};
+        }
+        else {
+            # encode content base64
+            $Attachment{Content} = MIME::Base64::encode_base64( $Attachment{Content} ),
+        }
+
+        # add
+        push(@AttachmentList, \%Attachment);
+    }
+
+    if ( scalar(@AttachmentList) == 1 ) {
+        return $Self->_Success(
+            Attachment => $AttachmentList[0],
+        );    
+    }
+
+    return $Self->_Success(
+        Attachment => \@AttachmentList,
+    );
+}
+
+1;
+
+
+
+
+=back
+
+=head1 TERMS AND CONDITIONS
+
+This software is part of the KIX project
+(L<http://www.kixdesk.com/>).
+
+This software comes with ABSOLUTELY NO WARRANTY. For details, see the enclosed file
+COPYING for license information (AGPL). If you did not receive this file, see
+
+<http://www.gnu.org/licenses/agpl.txt>.
+
+=cut
---
+++ Kernel/API/Operation/V1/Ticket/ArticleFlagSearch.pm
@@ -0,0 +1,194 @@
+# --
+# Kernel/API/Operation/User/ArticleAttachmentSearch.pm - API User Search operation backend
+# based upon Kernel/API/Operation/Ticket/TicketSearch.pm
+# original Copyright (C) 2001-2015 OTRS AG, http://otrs.com/
+# Copyright (C) 2006-2016 c.a.p.e. IT GmbH, http://www.cape-it.de
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::API::Operation::V1::Ticket::ArticleAttachmentSearch;
+
+use strict;
+use warnings;
+
+use Kernel::System::VariableCheck qw( :all );
+
+use base qw(
+    Kernel::API::Operation::V1::Ticket::Common
+);
+
+our $ObjectManagerDisabled = 1;
+
+=head1 NAME
+
+Kernel::API::Operation::Ticket::ArticleAttachmentSearch - API Ticket Article Attachment Search Operation backend
+
+=head1 PUBLIC INTERFACE
+
+=over 4
+
+=cut
+
+=item new()
+
+usually, you want to create an instance of this
+by using Kernel::API::Operation->new();
+
+=cut
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    my $Self = {};
+    bless( $Self, $Type );
+
+    # check needed objects
+    for my $Needed (qw(DebuggerObject WebserviceID)) {
+        if ( !$Param{$Needed} ) {
+            return $Self->_Error(
+                Code    => 'Operation.InternalError',
+                Message => "Got no $Needed!"
+            );
+        }
+
+        $Self->{$Needed} = $Param{$Needed};
+    }
+
+    return $Self;
+}
+
+=item Run()
+
+perform ArticleAttachmentSearch Operation. This will return a article attachment list.
+
+    my $Result = $OperationObject->Run(
+        Data => {
+        }
+    );
+
+    $Result = {
+        Success      => 1,                                # 0 or 1
+        Message => '',                               # In case of an error
+        Data         => {
+            Attachment => [
+                {
+                },
+                {
+                }
+            ]
+        },
+    };
+
+=cut
+
+sub Run {
+    my ( $Self, %Param ) = @_;
+
+    # init webservice
+    my $Result = $Self->Init(
+        WebserviceID => $Self->{WebserviceID},
+    );
+
+    if ( !$Result->{Success} ) {
+        $Self->_Error(
+            Code    => 'Webservice.InvalidConfiguration',
+            Message => $Result->{Message},
+        );
+    }
+
+    # prepare data
+    $Result = $Self->PrepareData(
+        Data       => $Param{Data},
+        Parameters => {
+            'TicketID' => {
+                Required => 1
+            },
+            'ArticleID' => {
+                Required => 1
+            },
+        }
+    );
+
+    # check result
+    if ( !$Result->{Success} ) {
+        return $Self->_Error(
+            Code    => 'Operation.PrepareDataError',
+            Message => $Result->{Message},
+        );
+    }
+
+    # check ticket permission
+    my $Permission = $Self->CheckAccessPermission(
+        TicketID => $Param{Data}->{TicketID},
+        UserID   => $Self->{Authorization}->{UserID},
+        UserType => $Self->{Authorization}->{UserType},
+    );
+
+    if ( !$Permission ) {
+        return $Self->_Error(
+            Code    => 'Object.NoPermission',
+            Message => "No permission to access ticket $Param{Data}->{TicketID}.",
+        );
+    }
+
+    my $TicketObject = $Kernel::OM->Get('Kernel::System::Ticket');
+
+    my %Article = $TicketObject->ArticleGet(
+        ArticleID  => $Param{Data}->{ArticleID},
+        UserID     => $Self->{Authorization}->{UserID},
+    );
+
+    # restrict article sender types
+    if ( $Self->{Authorization}->{UserType} eq 'Customer' && $Article{ArticleSenderType} ne 'customer') {
+        return $Self->_Error(
+            Code    => 'Object.NoPermission',
+            Message => "No permission to access article $Param{Data}->{ArticleID}.",
+        );
+    }    
+
+    # By default does not include HTML body as attachment (3) unless is explicitly requested (2).
+    my $StripPlainBodyAsAttachment = $Param{Data}->{HTMLBodyAsAttachment} ? 2 : 3;
+
+    my %AttachmentIndex = $TicketObject->ArticleAttachmentIndex(
+        ContentPath                => $Article{ContentPath},
+        ArticleID                  => $Param{Data}->{ArticleID},
+        StripPlainBodyAsAttachment => $StripPlainBodyAsAttachment,
+        UserID                     => $Self->{Authorization}->{UserID},
+    );
+
+    if ( %AttachmentIndex ) {
+
+        # get already prepared Article data from ArticleGet operation
+        my $AttachmentGetResult = $Self->ExecOperation(
+            OperationType => 'V1::Ticket::ArticleAttachmentGet',
+            Data          => {
+                TicketID     => $Param{Data}->{TicketID},
+                ArticleID    => $Param{Data}->{ArticleID},
+                AttachmentID => join(',', keys %AttachmentIndex),
+                include      => $Param{Data}->{include},
+                expand       => $Param{Data}->{expand},
+            }
+        );
+        if ( !IsHashRefWithData($AttachmentGetResult) || !$AttachmentGetResult->{Success} ) {
+            return $AttachmentGetResult;
+        }
+
+        my @ResultList = IsArrayRefWithData($AttachmentGetResult->{Data}->{Attachment}) ? @{$AttachmentGetResult->{Data}->{Attachment}} : ( $AttachmentGetResult->{Data}->{Attachment} );
+        
+        if ( IsArrayRefWithData(\@ResultList) ) {
+            return $Self->_Success(
+                Attachment => \@ResultList,
+            )
+        }
+    }
+
+    # return result
+    return $Self->_Success(
+        Attachment => [],
+    );
+}
+
+1;
