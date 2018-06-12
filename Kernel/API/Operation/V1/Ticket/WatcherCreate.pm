76de566231e01af7ae4a7eec408bd0900604a83e
ralf <ralf@herakles>
2018-06-12 17:34:32 +0200

    added watcher to ticket resource

---
+++ Kernel/API/Operation/V1/Ticket/WatcherCreate.pm
@@ -0,0 +1,183 @@
+# --
+# Modified version of the work: Copyright (C) 2006-2017 c.a.p.e. IT GmbH, http://www.cape-it.de
+# based on the original work of:
+# Copyright (C) 2001-2017 OTRS AG, http://otrs.com/
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::API::Operation::V1::Ticket::WatcherCreate;
+
+use strict;
+use warnings;
+
+use Kernel::System::VariableCheck qw( :all );
+
+use base qw(
+    Kernel::API::Operation::V1::Ticket::Common
+);
+
+our $ObjectManagerDisabled = 1;
+
+=head1 NAME
+
+Kernel::API::Operation::V1::Ticket::WatcherCreate - API WatcherCreate Operation backend
+
+=head1 SYNOPSIS
+
+=head1 PUBLIC INTERFACE
+
+=over 4
+
+=cut
+
+=item new()
+
+usually, you want to create an instance of this
+by using Kernel::API::Operation::V1->new();
+
+=cut
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    my $Self = {};
+    bless( $Self, $Type );
+
+    # check needed objects
+    for my $Needed (qw( DebuggerObject WebserviceID )) {
+        if ( !$Param{$Needed} ) {
+            return {
+                Success      => 0,
+                ErrorMessage => "Got no $Needed!"
+            };
+        }
+
+        $Self->{$Needed} = $Param{$Needed};
+    }
+
+    $Self->{Config} = $Kernel::OM->Get('Kernel::Config')->Get('API::Operation::V1::TicketWatcherCreate');
+
+    return $Self;
+}
+
+=item Run()
+
+perform WatcherCreate Operation. This will return the created WatcherItemID
+
+    my $Result = $OperationObject->Run(
+        Data => {
+            TicketID  => 123,                                        # required
+            Watcher => {                                         # required
+                WatchUserID => 123,                                # required
+            },
+        },
+    );
+
+    $Result = {
+        Success         => 1,                       # 0 or 1
+        Code            => '',                      #
+        Message         => '',                      # in case of error
+        Data            => {                        # result data payload after Operation
+            WatcherItemID => 1
+        },
+    };
+
+=cut
+
+sub Run {
+    my ( $Self, %Param ) = @_;
+
+    # init webservice
+    my $Result = $Self->Init(
+        WebserviceID => $Self->{WebserviceID},
+    );
+
+    if ( !$Result->{Success} ) {
+        $Self->_Error(
+            Code    => 'Webservice.InvalidConfiguration',
+            Message => $Result->{Message},
+        );
+    }
+
+#    # get possible item states
+#    my $PossibleItemStates = $Kernel::OM->Get('Kernel::Config')->Get('Ticket::Frontend::KIXSidebarWatcher')->{ItemState};
+#    my @PossibleItemStates = sort keys %{$PossibleItemStates};
+
+    # prepare data
+    $Result = $Self->PrepareData(
+        Data       => $Param{Data},
+        Parameters => {
+            'TicketID' => {
+                Required => 1
+            },
+            'Watcher::WatchUserID' => {
+                Required => 1
+            },
+        }
+    );
+
+    # check result
+    if ( !$Result->{Success} ) {
+        return $Self->_Error(
+            Code    => 'Operation.PrepareDataError',
+            Message => $Result->{Message},
+        );
+    }
+
+    # check write permission
+    my $Permission = $Self->CheckWritePermission(
+        TicketID => $Param{Data}->{TicketID},
+        UserID   => $Self->{Authorization}->{UserID},
+        UserType => $Self->{Authorization}->{UserType},
+    );
+
+    if ( !$Permission ) {
+        return $Self->_Error(
+            Code    => 'Object.NoPermission',
+            Message => "No permission to create Watcher item!",
+        );
+    }
+
+    # isolate and trim WatcherItem parameter
+    my $Watcher = $Self->_Trim(
+        Data => $Param{Data}->{Watcher},
+    );
+
+    my $Success = $Kernel::OM->Get('Kernel::System::Ticket')->TicketWatchSubscribe(
+        TicketID    => $Param{Data}->{TicketID},
+        WatchUserID => $Watcher->{WatchUserID},
+        UserID      => $Self->{Authorization}->{UserID},
+    );
+
+    if ( !$Success ) {
+        return $Self->_Error(
+            Code    => 'Object.UnableToCreate',
+            Message => 'Could not create Watcher item, please contact the system administrator',
+        );
+    }
+
+    return $Self->_Success(
+        Code            => 'Object.Created',
+        WatcherItemID => $Watcher->{WatchUserID},
+    );
+}
+
+1;
+
+
+=back
+
+=head1 TERMS AND CONDITIONS
+
+This software is part of the KIX project
+(L<http://www.kixdesk.com/>).
+
+This software comes with ABSOLUTELY NO WARRANTY. For details, see the enclosed file
+COPYING for license information (AGPL). If you did not receive this file, see
+
+<http://www.gnu.org/licenses/agpl.txt>.
+
+=cut
