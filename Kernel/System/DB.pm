8f0eb750fc1c2b66607f25ec33dc7c6d8d446022
Rene Böhm <rene.boehm@cape-it.de>
2019-08-14 15:19:39 +0200

    fixed value encoding in method FetchAllArrayRef in DB.pm

--- Kernel/System/DB.pm
+++ Kernel/System/DB.pm
@@ -824,8 +824,11 @@ sub FetchAllArrayRef {
         $Self->{LimitCounter}++;
     }
 
-    # return
+    # fetch
     my $Rows = $Self->{Cursor}->fetchall_arrayref();
+
+    # get encode object
+    my $EncodeObject = $Kernel::OM->Get('Kernel::System::Encode');
 
     # map columns
     my @Result;
@@ -834,33 +837,12 @@ sub FetchAllArrayRef {
         my $Counter = 0;
         foreach my $Column ( @{$Param{Columns}} ) {
             $RowData{$Column} = $Row->[$Counter++];
-        }
-        push(@Result, \%RowData);
-    }
-
-    if ( !$Self->{Backend}->{'DB::Encode'} ) {
-        return \@Result;
-    }
-
-    # get encode object
-    my $EncodeObject = $Kernel::OM->Get('Kernel::System::Encode');
-
-    # e. g. set utf-8 flag
-    ROW:
-    foreach my $Row (@{$Rows}) {
-        my $Counter = 0;
-        ELEMENT:
-        for my $Element (@{$Row}) {
-
-            next ELEMENT if !defined $Element;
-
-            if ( !defined $Self->{Encode} || ( $Self->{Encode} && $Self->{Encode}->[$Counter] ) ) {
-                $EncodeObject->EncodeInput( \$Element );
+            if ( $Self->{Backend}->{'DB::Encode'} ) {
+                # e. g. set utf-8 flag
+                $EncodeObject->EncodeInput( \$RowData{$Column} );
             }
         }
-        continue {
-            $Counter++;
-        }
+        push(@Result, \%RowData);
     }
 
     return \@Result;
