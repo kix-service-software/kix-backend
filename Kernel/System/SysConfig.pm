d06a922075902ee4b19a57fabe22061bf8febc07
René Böhm <rene.boehm@cape-it.de>
2019-12-20 09:20:07 +0100

    fixed bug that new SysConfig options would prevent the backend server from starting

--- Kernel/System/SysConfig.pm
+++ Kernel/System/SysConfig.pm
@@ -961,15 +961,6 @@ sub Rebuild {
             ValidID         => $OptionRaw->{Valid} == 1 ? 1 : 2,
         );
 
-        # ignore changed options
-        if ( $AllOptions{$Option{Name}}->{IsModified} || $AllOptions{$Option{Name}}->{ValidID} != $Option{ValidID} ) {
-            $Kernel::OM->Get('Kernel::System::Log')->Log(
-                Priority => 'info',
-                Message  => "Item \"$Option{Name}\" has been modified. Rebuild skipped.",
-            );    
-            next;            
-        }
-
         # check if this is a new option
         if ( !$AllOptions{ $Option{Name} } ) {
             # just import the new option
@@ -979,6 +970,15 @@ sub Rebuild {
             );
         }
         else {
+            # ignore changed options
+            if ( $AllOptions{$Option{Name}}->{IsModified} || $AllOptions{$Option{Name}}->{ValidID} != $Option{ValidID} ) {
+                $Kernel::OM->Get('Kernel::System::Log')->Log(
+                    Priority => 'info',
+                    Message  => "Item \"$Option{Name}\" has been modified. Rebuild skipped.",
+                );    
+                next;
+            }
+
             # we have to update the option
             my %ExistingOption = $Self->OptionGet(
                 Name    => $Option{Name},
