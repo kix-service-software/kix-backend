e511e16e33ae680c4be43d82876aaf864d53d2d9
Ricky Kaiser <ricky.kaiser@cape-it.de>
2020-03-03 14:41:01 +0100

    fixed missing values for hash-option with only one <Item> entry

--- Kernel/API/Operation/V1/Common.pm
+++ Kernel/API/Operation/V1/Common.pm
@@ -1912,7 +1912,7 @@ sub _ApplyInclude {
     # handle generic includes
     my $GenericIncludes = $Kernel::OM->Get('Kernel::Config')->Get('API::Operation::GenericInclude');
     if ( IsHashRefWithData($GenericIncludes) ) {
-        foreach my $Include ( keys %{ $Self->{Include} } ) {          
+        foreach my $Include ( keys %{ $Self->{Include} } ) {
             next if !$GenericIncludes->{$Include};
             next if $GenericIncludes->{$Include}->{IgnoreOperationRegEx} && $Self->{OperationType} =~ /$GenericIncludes->{$Include}->{IgnoreOperationRegEx}/;
 
@@ -1945,7 +1945,7 @@ sub _ApplyInclude {
                 if ( IsArrayRefWithData( $Param{Data}->{$Object} ) ) {
 
                     my $Index = 0;
-                    foreach my $ObjectID ( split( /\s*,\s*/, $Self->{RequestData}->{ $Self->{OperationConfig}->{ObjectID} } ) ) {                        
+                    foreach my $ObjectID ( split( /\s*,\s*/, $Self->{RequestData}->{ $Self->{OperationConfig}->{ObjectID} } ) ) {
 
                         $Param{Data}->{$Object}->[ $Index++ ]->{$Include} = $Self->{IncludeHandler}->{$IncludeHandler}->Run(
                             OperationConfig => $Self->{OperationConfig},
