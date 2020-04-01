c7b1b23852681ccb24b82bf5d94337ab98470cb1
Tobias Czirfusz <tobias.czirfusz@cape-it.de>
2020-04-01 15:03:12 +0200

    KIX2018-3656 show correct info on usertype customer for GET /session/user

--- Kernel/API/Operation/V1/Session/UserGet.pm
+++ Kernel/API/Operation/V1/Session/UserGet.pm
@@ -91,18 +91,10 @@ sub Run {
 
     # get the user data
     my %UserData;
-    if ( $Self->{Authorization}->{UserType} eq 'Agent' ) {
-        %UserData = $Kernel::OM->Get('Kernel::System::User')->GetUserData(
-            UserID        => $Self->{Authorization}->{UserID},
-            NoPreferences => 1
-        );
-    }
-    elsif ( $Self->{Authorization}->{UserType} eq 'Customer' ) {
-        %UserData = $Kernel::OM->Get('Kernel::System::Contact')->ContactGet(
-            ID            => $Self->{Authorization}->{UserID},
-            NoPreferences => 1
-        );
-    }
+    %UserData = $Kernel::OM->Get('Kernel::System::User')->GetUserData(
+        UserID        => $Self->{Authorization}->{UserID},
+        NoPreferences => 1
+    );
 
     if ( !IsHashRefWithData( \%UserData ) ) {
 
@@ -125,94 +117,91 @@ sub Run {
         }
     }
 
-    if ( $Self->{Authorization}->{UserType} eq 'Agent' ) {
+    # include preferences if requested - we can't do that with our generic sub-resource include function, because we don't have a UserID in our request
+    if ($Param{Data}->{include}->{Preferences}) {
 
-        # include preferences if requested - we can't do that with our generic sub-resource include function, because we don't have a UserID in our request
-        if ( $Param{Data}->{include}->{Preferences} ) {
+        # get already prepared preferences data from UserPreferenceSearch operation
+        my $Result = $Self->ExecOperation(
+            OperationType => 'V1::Session::UserPreferenceSearch',
+            Data          => {}
+        );
+        if (IsHashRefWithData($Result) && $Result->{Success}) {
+            $UserData{Preferences} = $Result->{Data}->{UserPreference};
+        }
+    }
 
-            # get already prepared preferences data from UserPreferenceSearch operation
-            my $Result = $Self->ExecOperation(
-                OperationType => 'V1::Session::UserPreferenceSearch',
-                Data          => {}
-            );
-            if ( IsHashRefWithData($Result) && $Result->{Success} ) {
-                $UserData{Preferences} = $Result->{Data}->{UserPreference};
-            }
+    # include tickets if requested
+    if ($Param{Data}->{include}->{Tickets}) {
+        my @TicketIDs;
+
+        my $TicketFilter;
+        if ($Param{Data}->{'Tickets.StateType'}) {
+            $TicketFilter = {
+                Field    => 'StateType',
+                Operator => 'IN',
+                Value    => [ split(/,/, $Param{Data}->{'Tickets.StateType'}) ],
+            };
+        }
+        elsif ($Param{Data}->{'Tickets.StateID'}) {
+            $TicketFilter = {
+                Field    => 'StateID',
+                Operator => 'IN',
+                Value    => [ split(/,/, $Param{Data}->{'Tickets.StateID'}) ],
+            };
         }
 
-        # include tickets if requested
-        if ( $Param{Data}->{include}->{Tickets} ) {
-            my @TicketIDs;
+        # get tickets owned by user
+        my $Tickets = $Self->_GetOwnedTickets(TicketFilter => $TicketFilter);
+        $UserData{Tickets}->{Owned} = $Tickets->{All};
+        $UserData{Tickets}->{OwnedAndUnseen} = $Tickets->{Unseen};
 
-            my $TicketFilter;
-            if ( $Param{Data}->{'Tickets.StateType'} ) {
-                $TicketFilter = {
-                    Field    => 'StateType',
-                    Operator => 'IN',
-                    Value    => [ split(/,/, $Param{Data}->{'Tickets.StateType'}) ],
-                };
-            }
-            elsif ( $Param{Data}->{'Tickets.StateID'} ) {
-                $TicketFilter = {
-                    Field    => 'StateID',
-                    Operator => 'IN',
-                    Value    => [ split(/,/, $Param{Data}->{'Tickets.StateID'}) ],
-                };
-            }
+        # get tickets owned by user and locked
+        $Tickets = $Self->_GetOwnedAndLockedTickets(TicketFilter => $TicketFilter);
+        $UserData{Tickets}->{OwnedAndLocked} = $Tickets->{All};
+        $UserData{Tickets}->{OwnedAndLockedAndUnseen} = $Tickets->{Unseen};
 
-            # get tickets owned by user
-            my $Tickets = $Self->_GetOwnedTickets(TicketFilter => $TicketFilter);
-            $UserData{Tickets}->{Owned}          = $Tickets->{All};
-            $UserData{Tickets}->{OwnedAndUnseen} = $Tickets->{Unseen};
+        # get tickets watched by user
+        $Tickets = $Self->_GetWatchedTickets(TicketFilter => $TicketFilter);
+        $UserData{Tickets}->{Watched} = $Tickets->{All};
+        $UserData{Tickets}->{WatchedAndUnseen} = $Tickets->{Unseen};
 
-            # get tickets owned by user and locked
-            $Tickets                                      = $Self->_GetOwnedAndLockedTickets(TicketFilter => $TicketFilter);
-            $UserData{Tickets}->{OwnedAndLocked}          = $Tickets->{All};
-            $UserData{Tickets}->{OwnedAndLockedAndUnseen} = $Tickets->{Unseen};
-
-            # get tickets watched by user
-            $Tickets                               = $Self->_GetWatchedTickets(TicketFilter => $TicketFilter);
-            $UserData{Tickets}->{Watched}          = $Tickets->{All};
-            $UserData{Tickets}->{WatchedAndUnseen} = $Tickets->{Unseen};
-
-            # force integer TicketIDs in response
-            foreach my $Type ( sort keys %{$UserData{Tickets}} ) {
-                my @TicketIDs = map { 0 + $_ } @{$UserData{Tickets}->{$Type}};
-                $UserData{Tickets}->{$Type} = \@TicketIDs;
-            }
-
-            # inform API caching about a new dependency
-            $Self->AddCacheDependency( Type => 'Ticket' );
-            $Self->AddCacheDependency( Type => 'Watcher' );
-            $Self->AddCacheDependency( Type => 'Contact' );
+        # force integer TicketIDs in response
+        foreach my $Type (sort keys %{$UserData{Tickets}}) {
+            my @TicketIDs = map {0 + $_} @{$UserData{Tickets}->{$Type}};
+            $UserData{Tickets}->{$Type} = \@TicketIDs;
         }
 
-        # include roleids if requested
-        if ( $Param{Data}->{include}->{RoleIDs} ) {
-
-            # get roles list
-            my @RoleList = $Kernel::OM->Get('Kernel::System::User')->RoleList(
-                UserID => $Self->{Authorization}->{UserID},
-            );
-            my @RoleIDs;
-            foreach my $RoleID ( sort @RoleList ) {
-                push( @RoleIDs, 0 + $RoleID );    # enforce nummeric ID
-            }
-            $UserData{RoleIDs} = \@RoleIDs;
-        }
-
-        #FIXME: workaoround KIX2018-3308
+        # inform API caching about a new dependency
+        $Self->AddCacheDependency(Type => 'Ticket');
+        $Self->AddCacheDependency(Type => 'Watcher');
         $Self->AddCacheDependency(Type => 'Contact');
-        my %ContactData = $Kernel::OM->Get('Kernel::System::Contact')->ContactGet(
+    }
+
+    # include roleids if requested
+    if ($Param{Data}->{include}->{RoleIDs}) {
+
+        # get roles list
+        my @RoleList = $Kernel::OM->Get('Kernel::System::User')->RoleList(
             UserID => $Self->{Authorization}->{UserID},
         );
-        $UserData{UserFirstname} = %ContactData ? $ContactData{Firstname} : undef;
-        $UserData{UserLastname} = %ContactData ? $ContactData{Lastname} : undef;
-        $UserData{UserFullname} = %ContactData ? $ContactData{Fullname} : undef;
-        $UserData{UserEmail} = %ContactData ? $ContactData{Email} : undef;
-        ###########################################################
-        $UserData{Contact} = (%ContactData) ? \%ContactData : undef;
+        my @RoleIDs;
+        foreach my $RoleID (sort @RoleList) {
+            push(@RoleIDs, 0 + $RoleID); # enforce nummeric ID
+        }
+        $UserData{RoleIDs} = \@RoleIDs;
     }
+
+    #FIXME: workaoround KIX2018-3308
+    $Self->AddCacheDependency(Type => 'Contact');
+    my %ContactData = $Kernel::OM->Get('Kernel::System::Contact')->ContactGet(
+        UserID => $Self->{Authorization}->{UserID},
+    );
+    $UserData{UserFirstname} = %ContactData ? $ContactData{Firstname} : undef;
+    $UserData{UserLastname} = %ContactData ? $ContactData{Lastname} : undef;
+    $UserData{UserFullname} = %ContactData ? $ContactData{Fullname} : undef;
+    $UserData{UserEmail} = %ContactData ? $ContactData{Email} : undef;
+    ###########################################################
+    $UserData{Contact} = (%ContactData) ? \%ContactData : undef;
 
     return $Self->_Success(
         User => \%UserData,
@@ -221,7 +210,7 @@ sub Run {
 
 sub _GetOwnedTickets {
     my ( $Self, %Param ) = @_;
-    my %Tickets;   
+    my %Tickets;
 
     my @Filter = (
         {
@@ -229,7 +218,7 @@ sub _GetOwnedTickets {
             Operator => 'EQ',
             Value    => $Self->{Authorization}->{UserID},
         }
-    );    
+    );
 
     if ( IsHashRefWithData($Param{TicketFilter}) ) {
         push(@Filter, $Param{TicketFilter});
@@ -261,14 +250,14 @@ sub _GetOwnedTickets {
                     Value  => '1',
                     UserID => $Self->{Authorization}->{UserID},
                 }
-                ]
+            ]
         }
-    );    
+    );
 
     if ( IsHashRefWithData($Param{TicketFilter}) ) {
         push(@Filter, $Param{TicketFilter});
     }
-    
+
     # execute ticket search
     my @SeenTicketIDs = $Kernel::OM->Get('Kernel::System::Ticket')->TicketSearch(
         Search => {
@@ -304,7 +293,7 @@ sub _GetOwnedAndLockedTickets {
             Operator => 'EQ',
             Value    => 2,
         }
-    );    
+    );
 
     if ( IsHashRefWithData($Param{TicketFilter}) ) {
         push(@Filter, $Param{TicketFilter});
@@ -340,9 +329,9 @@ sub _GetOwnedAndLockedTickets {
                     Value  => '1',
                     UserID => $Self->{Authorization}->{UserID},
                 }
-                ]
+            ]
         }
-    );    
+    );
 
     if ( IsHashRefWithData($Param{TicketFilter}) ) {
         push(@Filter, $Param{TicketFilter});
@@ -370,7 +359,7 @@ sub _GetOwnedAndLockedTickets {
 
 sub _GetWatchedTickets {
     my ( $Self, %Param ) = @_;
-    my %Tickets;       
+    my %Tickets;
 
     my @Filter = (
         {
@@ -378,7 +367,7 @@ sub _GetWatchedTickets {
             Operator => 'EQ',
             Value    => $Self->{Authorization}->{UserID},
         }
-    );    
+    );
 
     if ( IsHashRefWithData($Param{TicketFilter}) ) {
         push(@Filter, $Param{TicketFilter});
@@ -409,9 +398,9 @@ sub _GetWatchedTickets {
                     Value  => '1',
                     UserID => $Self->{Authorization}->{UserID},
                 }
-                ]
+            ]
         }
-    );    
+    );
 
     if ( IsHashRefWithData($Param{TicketFilter}) ) {
         push(@Filter, $Param{TicketFilter});
