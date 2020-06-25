98923d7a96a129d984e51163dd3891f20a4117df
Ricky Kaiser <ricky.kaiser@cape-it.de>
2020-06-25 09:01:01 +0200

    KIX2018-3921: fixed wrong recipients in notifications

---
+++ update/build-1231.pl
@@ -0,0 +1,93 @@
+#!/usr/bin/perl
+# --
+# Copyright (C) 2006-2019 c.a.p.e. IT GmbH, https://www.cape-it.de
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file LICENSE for license information (AGPL). If you
+# did not receive this file, see https://www.gnu.org/licenses/agpl.txt.
+# --
+
+use strict;
+use warnings;
+
+use File::Basename;
+use FindBin qw($Bin);
+use lib dirname($Bin);
+use lib dirname($Bin) . '/Kernel/cpan-lib';
+
+use Getopt::Std;
+use File::Path qw(mkpath);
+use Data::UUID;
+
+use Kernel::System::ObjectManager;
+use Kernel::System::VariableCheck qw(:all);
+use Kernel::System::EmailParser;
+
+# create object manager
+local $Kernel::OM = Kernel::System::ObjectManager->new(
+    'Log' => {
+        LogPrefix => 'framework_update-to-build-1231',
+    },
+);
+my $LogObject = $Kernel::OM->Get('Log');
+
+use vars qw(%INC);
+
+# fix wrong recipient name in existing notifications
+_FixRecipientInExistingNotifications();
+
+sub _FixRecipientInExistingNotifications {
+    my ( $Self, %Param ) = @_;
+
+    $Self->{NotificationEventObject} = $Kernel::OM->Get('NotificationEvent');
+
+    # get all current notifications
+    my %NotificationList = $Self->{NotificationEventObject}->NotificationList(
+        Type    => 'Ticket',
+        Details => 1,
+        All     => 1,
+    );
+
+    # update existing notifications (missing s on some recipients)
+    NOTIFICATION:
+    for my $Key ( keys %NotificationList ) {
+        next NOTIFICATION if !IsHashRefWithData( $NotificationList{$Key}->{Data} );
+        next NOTIFICATION if !IsArrayRefWithData( $NotificationList{$Key}->{Data}->{Recipients} );
+
+        # collect fixed recipient
+        my @FixedRecipients = ();
+        my $UpdateNeeded = 0;
+        for my $Recipient ( @{ $NotificationList{$Key}->{Data}->{Recipients} } ) {
+            if ( $Recipient =~ m/^Agent(Write|Read)Permission$/ ) {
+                $UpdateNeeded = 1;
+                $Recipient .= 's';
+            }
+            push(@FixedRecipients, $Recipient);
+        }
+
+        if ($UpdateNeeded) {
+            $NotificationList{$Key}->{Data}->{Recipients} = \@FixedRecipients;
+
+            my $Ok = $Self->{NotificationEventObject}->NotificationUpdate(
+                %{ $NotificationList{$Key} },
+                UserID  => 1,
+            );
+        }
+    }
+
+    return 1;
+}
+
+exit 0;
+
+=head1 TERMS AND CONDITIONS
+
+This software is part of the KIX project
+(L<https://www.kixdesk.com/>).
+
+This software comes with ABSOLUTELY NO WARRANTY. For details, see the enclosed file
+LICENSE for license information (AGPL). If you did not receive this file, see
+
+<https://www.gnu.org/licenses/agpl.txt>.
+
+=cut
