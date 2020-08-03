ee9aff90ca44d47d49acc4921d1c9ee812a07fc2
Rene Böhm <rene.boehm@cape-it.de>
2020-08-03 11:21:02 +0200

    KIX2018-4225: fixed issue with unknown Contact in NewTicket

--- Kernel/System/PostMaster/NewTicket.pm
+++ Kernel/System/PostMaster/NewTicket.pm
@@ -167,67 +167,54 @@ sub Run {
         if ( %List ) {
             $GetParam{'X-KIX-Contact'} = (keys %List)[0];
         }
+        else {
+            # we can't assign a contact to it, clear it to give was to the fallback
+            $GetParam{'X-KIX-Contact'} = undef;
+        }
     }
 
-    # if no contact found, get contact form From: (sender address)
-    if ( !$GetParam{'X-KIX-Contact'} ) {
+    # if no contact found, get contact from sender address
+    if ( !$GetParam{'X-KIX-Contact'} && $GetParam{SenderEmailAddress} ) {
 
         my %ContactData;
-        if ( $GetParam{From} ) {
 
-            my @EmailAddresses = $Self->{ParserObject}->SplitAddressLine(
-                Line => $GetParam{From},
+        # get customer user object
+        my $ContactObject = $Kernel::OM->Get('Contact');
+
+        my %List = $ContactObject->ContactSearch(
+            PostMasterSearch => lc( $GetParam{SenderEmailAddress} ),
+            Limit            => 1,
+            Valid            => 0
+        );
+
+        if ( %List ) {
+            %ContactData = $ContactObject->ContactGet(
+                ID => (keys %List)[0],
+            );
+        }
+
+        # create a new contact if no existing contact was found for the given "from"
+        if ( !$ContactData{ID} ) {
+            my $ParserObject = Kernel::System::EmailParser->new(
+                Mode => 'Standalone',
+            );
+            my $ContactEmailRealname = $ParserObject->GetRealname(
+                Email => $GetParam{SenderEmailAddress}
+            );
+            my @NameChunks = split(' ', $ContactEmailRealname);
+
+            my $ContactID = $ContactObject->ContactAdd(
+                Firstname             => (@NameChunks) ? $NameChunks[0] : 'not',
+                Lastname              => (@NameChunks) ? join(" ", splice(@NameChunks, 1)) : 'assigned',
+                Email                 => lc( $GetParam{SenderEmailAddress} ),
+                PrimaryOrganisationID => $GetParam{'SenderEmailAddress'},
+                ValidID               => 1,
+                UserID                => 1,
             );
 
-            for my $Address (@EmailAddresses) {
-                $GetParam{EmailFrom} = $Self->{ParserObject}->GetEmailAddress(
-                    Email => $Address,
-                );
-            }
-
-            if ( $GetParam{EmailFrom} ) {
-
-                # get customer user object
-                my $ContactObject = $Kernel::OM->Get('Contact');
-
-                my %List = $ContactObject->ContactSearch(
-                    PostMasterSearch => lc( $GetParam{EmailFrom} ),
-                    Limit            => 1,
-                    Valid            => 0
-                );
-
-                if ( %List ) {
-                    %ContactData = $ContactObject->ContactGet(
-                        ID => (keys %List)[0],
-                    );
-                }
-
-                # create a new contact if no existing contact was found for the given "from"
-                if ( !$ContactData{ID} ) {
-                    my $ParserObject = Kernel::System::EmailParser->new(
-                        Mode => 'Standalone',
-                    );
-                    my $ContactEmailRealname = $ParserObject->GetRealname(
-                        Email => $GetParam{EmailFrom}
-                    );
-                    my @NameChunks = split(' ', $ContactEmailRealname);
-
-                    my $ContactID = $Kernel::OM->Get('Contact')->ContactAdd(
-                        Firstname             => (@NameChunks) ? $NameChunks[0] : 'not',
-                        Lastname              => (@NameChunks) ? join(" ", splice(@NameChunks, 1)) : 'assigned',
-                        Email                 => lc( $GetParam{EmailFrom} ),
-                        PrimaryOrganisationID => undef,
-                        ValidID               => 1,
-                        UserID                => 1,
-                    );
-
-                    my $ContactObject = $Kernel::OM->Get('Contact');
-
-                    %ContactData = $ContactObject->ContactGet(
-                        ID => $ContactID
-                    );
-                }
-            }
+            %ContactData = $ContactObject->ContactGet(
+                ID => $ContactID
+            );
         }
 
         # take PrimaryOrganisationID from contact lookup or from "from" field
@@ -237,7 +224,7 @@ sub Run {
             # notice that Login is from contact data
             $Kernel::OM->Get('Log')->Log(
                 Priority => 'notice',
-                Message  => "Take Login ($ContactData{ID}) from contact based on ($GetParam{'EmailFrom'}).",
+                Message  => "Take Login ($ContactData{ID}) from contact based on ($GetParam{'SenderEmailAddress'}).",
             );
         }
         if ( $ContactData{PrimaryOrganisationID} && !$GetParam{'X-KIX-Organisation'} ) {
@@ -247,7 +234,7 @@ sub Run {
             $Kernel::OM->Get('Log')->Log(
                 Priority => 'notice',
                 Message  => "Take PrimaryOrganisationID ($ContactData{PrimaryOrganisationID})"
-                    . " from contact based on ($GetParam{'EmailFrom'}).",
+                    . " from contact based on ($GetParam{'SenderEmailAddress'}).",
             );
         }
     }
