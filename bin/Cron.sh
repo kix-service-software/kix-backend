479541e0afe17593df83be1c284ba014e487116e
rabo <ralf.boehm@cape-it.de>
2020-08-03 09:10:27 +0200

    Revert "added files"
    This reverts commit d2364e779857693378b0052c2319823b4e19c4e4.

---
+++ bin/Cron.sh
@@ -0,0 +1,102 @@
+#!/bin/sh
+# --
+# Modified version of the work: Copyright (C) 2006-2020 c.a.p.e. IT GmbH, http://www.cape-it.de
+# based on the original work of:
+# Copyright (C) 2001-2017 OTRS AG, http://otrs.com/
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+CURRENTUSER=`whoami`
+CRON_USER="$2"
+
+# check if a common user try to use -u
+if test -n "$CRON_USER"; then
+    if test $CURRENTUSER != root; then
+        echo "Run this script just as webserver user! Or use 'Cron.sh {start|stop|restart} <webserver user>' as root!"
+        exit 5
+    fi
+fi
+
+# check if the cron user is specified
+if test -z "$CRON_USER"; then
+    if test $CURRENTUSER = root; then
+        echo "Run this script just as webserver user! Or use 'Cron.sh {start|stop|restart} <webserver user>' as root!"
+        exit 5
+    fi
+fi
+
+# find otrs root
+cd "`dirname $0`/../"
+KIX_HOME="`pwd`"
+cd -
+
+if test -e $KIX_HOME/var/cron; then
+    KIX_ROOT=$KIX_HOME
+else
+    echo "No cronjobs in $KIX_HOME/var/cron found!";
+    echo " * Check the KIX_HOME environment variable and your KIX system configuration.";
+    exit 5;
+fi
+
+CRON_DIR=$KIX_ROOT/var/cron
+CRON_TMP_FILE=$KIX_ROOT/var/tmp/otrs-cron-tmp.$$
+
+echo "Cron.sh - start/stop KIX cronjobs"
+
+#
+# main part
+#
+case "$1" in
+    # ------------------------------------------------------
+    # start
+    # ------------------------------------------------------
+    start)
+        # add -u to cron user if user exists
+        if test -n "$CRON_USER"; then
+            CRON_USER=" -u $CRON_USER"
+        fi
+
+        if mkdir -p $CRON_DIR; cd $CRON_DIR && ls -d * | grep -Ev "(\.(dist|rpm|bak|backup|custom_backup|save|swp)|\~)$" | xargs cat > $CRON_TMP_FILE && crontab $CRON_USER $CRON_TMP_FILE; then
+
+            rm -rf $CRON_TMP_FILE
+            echo "(using $KIX_ROOT) done";
+            exit 0;
+        else
+            echo "failed";
+            exit 1;
+        fi
+    ;;
+    # ------------------------------------------------------
+    # stop
+    # ------------------------------------------------------
+    stop)
+        # add -u to cron user if user exists
+        if test -n "$CRON_USER"; then
+            CRON_USER=" -u $CRON_USER"
+        fi
+
+        if crontab $CRON_USER -r ; then
+            echo "done";
+            exit 0;
+        else
+            echo "failed";
+            exit 1;
+        fi
+    ;;
+    # ------------------------------------------------------
+    # restart
+    # ------------------------------------------------------
+    restart)
+        $0 stop "$CRON_USER"
+        $0 start "$CRON_USER"
+    ;;
+    # ------------------------------------------------------
+    # Usage
+    # ------------------------------------------------------
+    *)
+    echo "Usage: $0 {start|stop|restart}"
+    exit 1
+esac
---
+++ bin/kix.Console.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/perl
+# --
+# Modified version of the work: Copyright (C) 2006-2020 c.a.p.e. IT GmbH, http://www.cape-it.de
+# based on the original work of:
+# Copyright (C) 2001-2017 OTRS AG, http://otrs.com/
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+use strict;
+use warnings;
+
+use File::Basename;
+use FindBin qw($RealBin);
+use lib dirname($RealBin);
+use lib dirname($RealBin) . '/Kernel/cpan-lib';
+use lib dirname($RealBin) . '/plugins';
+
+use Kernel::System::ObjectManager;
+
+local $Kernel::OM = Kernel::System::ObjectManager->new(
+    'Log' => {
+        LogPrefix => 'kix.Console.pl',
+    },
+);
+
+exit $Kernel::OM->Get('Console')->Run(@ARGV);
+
+
+
+
+=back
+
+=head1 TERMS AND CONDITIONS
+
+This software is part of the KIX project
+(L<https://www.kixdesk.com/>).
+
+This software comes with ABSOLUTELY NO WARRANTY. For details, see the enclosed file
+LICENSE-GPL3 for license information (GPL3). If you did not receive this file, see
+
+<https://www.gnu.org/licenses/gpl-3.0.txt>.
+
+=cut
