ef6a1e23b861ae31615f178370f58b3a8b6161a0
Ricky Kaiser <ricky.kaiser@cape-it.de>
2021-11-29 14:08:51 +0100

    KIX2018-6150: adjust some foreign keys in database

---
+++ update/build-1528.pl
@@ -0,0 +1,62 @@
+#!/usr/bin/perl
+# --
+# Copyright (C) 2006-2021 c.a.p.e. IT GmbH, https://www.cape-it.de
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file LICENSE for license information (AGPL). If you
+# did not receive this file, see https://www.gnu.org/licenses/agpl.txt.
+# --
+
+use strict;
+use warnings;
+
+use File::Basename;
+use FindBin qw($Bin);
+use lib dirname($Bin);
+use lib dirname($Bin) . '/Kernel/cpan-lib';
+
+use Kernel::System::ObjectManager;
+use Kernel::System::VariableCheck qw(:all);
+use Kernel::System::Role::Permission;
+
+# create object manager
+local $Kernel::OM = Kernel::System::ObjectManager->new(
+    'Log' => {
+        LogPrefix => 'framework_update-to-build-1528',
+    },
+);
+
+use vars qw(%INC);
+
+_RemoveAbandonedJobLogs();
+
+exit 0;
+
+sub _RemoveAbandonedJobLogs {
+    my ( $Self, %Param ) = @_;
+
+    return if !$Kernel::OM->Get('DB')->Do(
+        SQL  => 'DELETE FROM automation_log WHERE job_id NOT IN (select job.id FROM job)'
+    );
+
+    # delete whole cache
+    $Kernel::OM->Get('Cache')->CleanUp();
+
+    return 1;
+}
+
+exit 0;
+
+=back
+
+=head1 TERMS AND CONDITIONS
+
+This software is part of the KIX project
+(L<https://www.kixdesk.com/>).
+
+This software comes with ABSOLUTELY NO WARRANTY. For details, see the enclosed file
+LICENSE for license information (AGPL). If you did not receive this file, see
+
+<https://www.gnu.org/licenses/agpl.txt>.
+
+=cut
