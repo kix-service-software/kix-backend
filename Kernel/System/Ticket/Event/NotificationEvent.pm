9f905ca1ffefc33b78444ee06db9252751b14a42
Rene Böhm <rene.boehm@cape-it.de>
2022-09-29 10:16:01 +0200

    added restriction of parallel instances in async notifications

--- Kernel/System/Ticket/Event/NotificationEvent.pm
+++ Kernel/System/Ticket/Event/NotificationEvent.pm
@@ -91,9 +91,11 @@ my $StartTime = Time::HiRes::time();
     my $Result;
 
     if ( !$ENV{IsDaemon} && $Kernel::OM->Get('Config')->Get('TicketNotification::SendAsynchronously') ) {
+        my $MaximumParallelInstances = $Kernel::OM->Get('Config')->Get('TicketNotification::SendAsynchronously::MaximumParallelInstances') || 0;
         my $Result = $Self->AsyncCall(
-            FunctionName   => '_Run',
-            FunctionParams => \%Param,
+            FunctionName             => '_Run',
+            FunctionParams           => \%Param,
+            MaximumParallelInstances => $MaximumParallelInstances
         );
         if ( !$Result ) {
             $Kernel::OM->Get('Log')->Log(
