585b6c3b58ce502fcad76601879f5d2b3fbbd90d
Ricky Kaiser <ricky.kaiser@cape-it.de>
2023-04-24 13:05:24 +0200

    Revert "Merge branch 'KIX2018-9209' into 'rel-18_FEATURE'"

--- Kernel/System/EmailParser.pm
+++ Kernel/System/EmailParser.pm
@@ -1,5 +1,5 @@
 # --
-# Modified version of the work: Copyright (C) 2006-2023 KIX Service Software GmbH, https://www.kixdesk.com
+# Modified version of the work: Copyright (C) 2006-2023 KIX Service Software GmbH, https://www.kixdesk.com 
 # based on the original work of:
 # Copyright (C) 2001-2017 OTRS AG, https://otrs.com/
 # --
@@ -664,13 +664,9 @@ sub PartsAttachments {
     my %Data = $Self->GetContentTypeParams( ContentType => $PartData{ContentType} );
     if ( $Data{Charset} ) {
         $PartData{Charset} = $Data{Charset};
-    } else {
-        $PartData{Charset} = '';
     }
-
-    # remove additional data
-    if ($Data{ContentType}) {
-        $PartData{ContentType} = $Data{ContentType};
+    else {
+        $PartData{Charset} = '';
     }
 
     # get content (if possible)
@@ -707,11 +703,6 @@ sub PartsAttachments {
             );
             if ( $Data{Charset} ) {
                 $PartData{Charset} = $Data{Charset};
-            }
-
-            # remove additional data
-            if ( $Data{ContentType} ) {
-                $PartData{ContentDisposition} = $Data{ContentType};
             }
         }
         else {
@@ -901,10 +892,6 @@ sub GetContentTypeParams {
     if ( $Param{ContentType} =~ /Content-Type:\s{0,1}(.+?\/.+?)(;|'|"|\s)/i ) {
         $Param{MimeType} = $1;
         $Param{MimeType} =~ s/"|'//g;
-    }
-    my @Parts = split(/;\s*/, $Param{ContentType});
-    if (@Parts > 1) {
-        $Param{ContentType} = $Parts[0];
     }
     return %Param;
 }
