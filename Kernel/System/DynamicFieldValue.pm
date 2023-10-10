c19307cef5b9c4bdd868ebba578f792da7214252
Ricky Kaiser <ricky.kaiser@kixdesk.com>
2023-10-10 15:37:55 +0200

    KIX2018-10374: do not accept years greater than 2200

--- Kernel/System/DynamicFieldValue.pm
+++ Kernel/System/DynamicFieldValue.pm
@@ -1,5 +1,5 @@
 # --
-# Modified version of the work: Copyright (C) 2006-2023 KIX Service Software GmbH, https://www.kixdesk.com 
+# Modified version of the work: Copyright (C) 2006-2023 KIX Service Software GmbH, https://www.kixdesk.com
 # based on the original work of:
 # Copyright (C) 2001-2017 OTRS AG, https://otrs.com/
 # --
@@ -443,7 +443,7 @@ sub ValueValidate {
     if ( $Value{ValueDateTime} ) {
 
         if (
-            $Value{ValueDateTime} !~ m/\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}/ &&
+            $Value{ValueDateTime} !~ m/^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}/ &&
             $Value{ValueDateTime} !~ m/[+-]\d+[YMwdhms]/
         ) {
             return if $Param{Silent};
@@ -451,6 +451,14 @@ sub ValueValidate {
             $Kernel::OM->Get('Log')->Log(
                 Priority => 'error',
                 Message  => "Invalid DateTime string \"$Value{ValueDateTime}\"!"
+            );
+            return;
+        }
+
+        if ($Value{ValueDateTime} =~ m/^(\d+)-/ && 2200 < $1) {
+            $Kernel::OM->Get('Log')->Log(
+                Priority => 'error',
+                Message  => "Years greater than 2200 are not possible!"
             );
             return;
         }
