3533cbc1ea9ca66186d0cb1ad122820319b52436
Frank Jacquemin <frank.jacquemin@cape-it.de>
2023-11-09 13:48:20 +0100

    update api sort

--- Kernel/API/Operation/V1/CMDB/ConfigItemSearch.pm
+++ Kernel/API/Operation/V1/CMDB/ConfigItemSearch.pm
@@ -29,6 +29,16 @@ Kernel::API::Operation::CMDB::ConfigItemSearch - API CMDB Search Operation backe
 
 =cut
 
+sub Init {
+    my ( $Self, %Param ) = @_;
+
+    my $Result = $Self->SUPER::Init(%Param);
+
+    $Self->{HandleSortInCORE} = 1;
+
+    return $Result;
+}
+
 =item Run()
 
 perform ConfigItemSearch Operation. This will return a class list.
