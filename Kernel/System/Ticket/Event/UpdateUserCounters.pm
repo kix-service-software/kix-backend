c197345609cee5a6c579844dd9fadcbb739779db
René Böhm <rene.boehm@kixdesk.com>
2024-01-15 11:49:35 +0100

    KIX2018-10850: fixed counter issue

--- Kernel/System/Ticket/Event/UpdateUserCounters.pm
+++ Kernel/System/Ticket/Event/UpdateUserCounters.pm
@@ -103,10 +103,11 @@ sub Run {
         };
 
         if ( !$Self->{ViewableStates}->{$Ticket{StateType}} ) {
-            # delete all user counters for this object for all users and leave
+            # delete all user counters for this object for the ticket owner and leave
             $Kernel::OM->Get('User')->DeleteUserCounterObject(
                 Category => 'Ticket',
                 ObjectID => $Param{Data}->{TicketID},
+                UserID   => $Ticket{OwnerID}
             );
             return 1;
         }
@@ -370,6 +371,13 @@ sub HandleTicketOwnerUpdate {
 
 sub HandleTicketSubscribe {
     my ($Self, %Param) = @_;
+
+    my $IsWatched = $Kernel::OM->Get('Watcher')->WatcherLookup(
+        Object      => 'Ticket',
+        ObjectID    => $Param{Ticket}->{TicketID},
+        WatchUserID => $Param{Data}->{WatchUserID}
+    );
+    return if !$IsWatched;
 
     $Kernel::OM->Get('User')->AddUserCounterObject(
         Category => 'Ticket',
