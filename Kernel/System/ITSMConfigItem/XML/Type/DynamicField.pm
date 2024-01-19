746a0719835edbedb0346915d453ab2161cef3b1
Mario Bauer <mario.bauer@kixdesk.com>
2024-01-19 08:15:25 +0100

    KIX2018-10574: cleanup obsolete functions

--- Kernel/System/ITSMConfigItem/XML/Type/DynamicField.pm
+++ Kernel/System/ITSMConfigItem/XML/Type/DynamicField.pm
@@ -89,53 +89,6 @@ sub ValueLookup {
     return $Value;
 }
 
-=item StatsAttributeCreate()
-
-create a attribute array for the stats framework
-
-    my $Attribute = $BackendObject->StatsAttributeCreate(
-        Key  => 'Key::Subkey',
-        Name => 'Name',
-        Item => $ItemRef,
-    );
-
-=cut
-
-sub StatsAttributeCreate {
-    my ( $Self, %Param ) = @_;
-
-    # check needed stuff
-    for my $Argument (qw(Key Name Item)) {
-        if ( !$Param{$Argument} ) {
-            $Self->{LogObject}->Log(
-                Priority => 'error',
-                Message  => "Need $Argument!",
-            );
-            return;
-        }
-    }
-
-    # get item list
-    my $ItemList = $Self->{DynamicFieldObject}->DynamicFieldGet(
-        Name => $Param{Item}->{Input}->{Name} || '',
-    );
-
-    # create arrtibute
-    my $Attribute = [
-        {
-            Name             => $Param{Name},
-            UseAsXvalue      => 1,
-            UseAsValueSeries => 1,
-            UseAsRestriction => 1,
-            Element          => $Param{Key},
-            Block            => 'MultiSelectField',
-            Values           => $ItemList->{Config}->{PossibleValues} || {},
-        },
-    ];
-
-    return $Attribute;
-}
-
 =item ExportSearchValuePrepare()
 
 prepare search value for export
