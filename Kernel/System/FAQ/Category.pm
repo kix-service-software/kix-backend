1598bcfecc9304e9f51a77f6ca77d03fbaa83b4a
Frank Jacquemin <frank.jacquemin@cape-it.de>
2024-04-08 07:22:04 +0200

    KIX2018-11103 - fixed missing delete of object search cache after delete faq

--- Kernel/System/FAQ/Category.pm
+++ Kernel/System/FAQ/Category.pm
@@ -1,5 +1,5 @@
 # --
-# Modified version of the work: Copyright (C) 2006-2024 KIX Service Software GmbH, https://www.kixdesk.com 
+# Modified version of the work: Copyright (C) 2006-2024 KIX Service Software GmbH, https://www.kixdesk.com
 # based on the original work of:
 # Copyright (C) 2001-2017 OTRS AG, https://otrs.com/
 # --
@@ -271,6 +271,11 @@ sub CategoryDelete {
     # clear cache
     $Kernel::OM->Get('Cache')->CleanUp(
         Type => $Self->{CacheType},
+    );
+
+    # reset cache object search
+    $Kernel::OM->Get('Cache')->CleanUp(
+        Type => $Self->{OSCacheType},
     );
 
     # push client callback event
