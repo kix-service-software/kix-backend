75b1018716cb76aa97cb231a3fdfa88f0ee08cbb
Frank Jacquemin <frank.jacquemin@cape-it.de>
2024-04-09 11:01:47 +0200

    Draft: KIX2018-11257 - changed SMIME handling

---
+++ Kernel/System/SMIME.pm
@@ -0,0 +1,124 @@
+# --
+# Copyright (C) 2006-2024 KIX Service Software GmbH, https://www.kixdesk.com
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file LICENSE-AGPL for license information (AGPL). If you
+# did not receive this file, see https://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::System::SMIME;
+
+use strict;
+use warnings;
+
+our @ObjectDependencies = qw(
+    Config
+    Cache
+);
+
+=head1 NAME
+
+Kernel::System::SMIME - smime backend lib
+
+=head1 SYNOPSIS
+
+This is a sub module of Kernel::System::SMIME contains all smime functions.
+
+=head1 PUBLIC INTERFACE
+
+=over 4
+
+=cut
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = {};
+    bless( $Self, $Type );
+
+    $Self->{Debug} = $Param{Debug} || 0;
+
+    return $Self;
+}
+
+
+=item CertificateCreate()
+
+create a local certificate
+
+    my $Success = $CryptObject->CertificateCreate(
+        ## TODO: filling with parameters
+    );
+
+    return boolean?
+
+=cut
+
+sub CertificateCreate {
+    my ( $Self, %Param ) = @_;
+
+   return 1;
+}
+
+=item CertificateGet()
+
+get a local certificate
+
+    my $Certificate = $CryptObject->CertificateGet(
+
+    );
+
+=cut
+
+sub CertificateGet {
+    my ( $Self, %Param ) = @_;
+
+    return {};
+}
+
+=item CertificateDelete()
+
+remove a local certificate
+
+    $CryptObject->CertificateDelete(
+
+    );
+
+=cut
+
+sub CertificateDelete {
+    my ( $Self, %Param ) = @_;
+
+    return 1;
+}
+
+=item CertificateList()
+
+get list of local certificates filenames
+
+    my @CertList = $CryptObject->CertificateList();
+
+=cut
+
+sub CertificateList {
+    my ( $Self, %Param ) = @_;
+
+    my @CertList;
+
+    return @CertList;
+}
+
+=back
+
+=head1 TERMS AND CONDITIONS
+
+This software is part of the KIX project
+(L<https://www.kixdesk.com/>).
+
+This software comes with ABSOLUTELY NO WARRANTY. For details, see the enclosed file
+LICENSE-AGPL for license information (AGPL). If you did not receive this file, see
+
+<https://www.gnu.org/licenses/agpl.txt>.
+
+=cut
