ebf6de4ae11bc584a359521309ecf0645ddf488a
Ricky Kaiser <ricky.kaiser@kixdesk.com>
2024-11-01 07:51:15 +0100

    KIX2018-11456: add access to pendingtime for customers

---
+++ update/build-2029.pl
@@ -0,0 +1,119 @@
+#!/usr/bin/perl
+# --
+# Copyright (C) 2006-2024 KIX Service Software GmbH, https://www.kixdesk.com
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file LICENSE for license information (AGPL). If you
+# did not receive this file, see https://www.gnu.org/licenses/agpl.txt.
+# --
+
+use strict;
+use warnings;
+
+use File::Basename;
+use FindBin qw($Bin);
+use lib dirname($Bin);
+use lib dirname($Bin) . '/Kernel/cpan-lib';
+
+use Kernel::System::ObjectManager;
+use Kernel::System::VariableCheck qw(:all);
+
+# create object manager
+local $Kernel::OM = Kernel::System::ObjectManager->new(
+    'Log' => {
+        LogPrefix => 'framework_update-to-build-2029',
+    },
+);
+
+use vars qw(%INC);
+
+_UpdateCustomerRolePermission();
+
+exit 0;
+
+sub _UpdateCustomerRolePermission {
+
+    my $RoleObject = $Kernel::OM->Get('Role');
+
+    my %RoleList = reverse $RoleObject->RoleList();
+    my %PermissionTypeList = reverse $RoleObject->PermissionTypeList();
+
+    my @PermissionUpdates = (
+        {
+            Permission => {
+                Role   => 'Customer',
+                Type   => 'Property',
+                Target => '/tickets/*{Ticket.[Age,Articles,Changed,ContactID,Created,CreateTimeUnix,DynamicFields,OrganisationID,PriorityID,QueueID,StateID,TypeID,TicketNumber,Title]}'
+            },
+            Change     => {
+                Target => '/tickets/*{Ticket.[Age,Articles,Changed,ContactID,Created,CreateTimeUnix,DynamicFields,OrganisationID,PriorityID,QueueID,StateID,TypeID,TicketNumber,Title,PendingTime]}'
+            }
+        },
+    );
+
+    foreach my $Update (@PermissionUpdates) {
+        my $RoleID = $RoleList{$Update->{Permission}->{Role}};
+        if (!$RoleID) {
+            $Kernel::OM->Get('Log')->Log(
+                Priority => 'error',
+                Message  => 'Unable to find role "' . $Update->{Permission}->{Role} . '"!'
+            );
+            next;
+        }
+        my $PermissionTypeID = $PermissionTypeList{$Update->{Permission}->{Type}};
+        if (!$PermissionTypeID) {
+            $Kernel::OM->Get('Log')->Log(
+                Priority => 'error',
+                Message  => 'Unable to find permission type "' . $Update->{Permission}->{Type} . '"!'
+            );
+            next;
+        }
+
+        my $PermissionID = $RoleObject->PermissionLookup(
+            RoleID => $RoleID,
+            TypeID => $PermissionTypeID,
+            Target => $Update->{Permission}->{Target}
+        );
+        next if !$PermissionID;
+
+        my $Success = $RoleObject->PermissionUpdate(
+            ID     => $PermissionID,
+            UserID => 1,
+            %{$Update->{Change}}
+        );
+
+        if (!$Success) {
+            $Kernel::OM->Get('Log')->Log(
+                Priority => 'error',
+                Message  => "Unable to update permission (role=$Update->{Permission}->{Role}, type=$Update->{Permission}->{Type}, target=$Update->{Permission}->{Target})!"
+            );
+        }
+        else {
+            $Kernel::OM->Get('Log')->Log(
+                Priority => 'info',
+                Message  => "Updated permission ID $PermissionID!"
+            );
+        }
+    }
+
+    # delete whole cache
+    $Kernel::OM->Get('Cache')->CleanUp();
+
+    return 1;
+}
+
+exit 0;
+
+=back
+
+=head1 TERMS AND CONDITIONS
+
+This software is part of the KIX project
+(L<https://www.kixdesk.com/>).
+
+This software comes with ABSOLUTELY NO WARRANTY. For details, see the enclosed file
+LICENSE for license information (AGPL). If you did not receive this file, see
+
+<https://www.gnu.org/licenses/agpl.txt>.
+
+=cut
