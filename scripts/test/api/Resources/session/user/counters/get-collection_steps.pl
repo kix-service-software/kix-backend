f4884993aaa784fd377d5f662b3fabba8993053c
ralf boehm <ralf.boehm@cape-it.de>
2025-05-07 15:41:44 +0200

    added change counter test

---
+++ scripts/test/api/Resources/session/user/counters/get-collection_steps.pl
@@ -0,0 +1,43 @@
+# --
+# Copyright (C) 2006-2025 KIX Service Software GmbH, https://www.kixdesk.com/
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file LICENSE-AGPL for license information (AGPL). If you
+# did not receive this file, see https://www.gnu.org/licenses/agpl.txt.
+# --
+use warnings;
+
+use Cwd;
+use lib cwd();
+use lib cwd() . '/Kernel/cpan-lib';
+use lib cwd() . '/plugins';
+use lib cwd() . '/scripts/test/api/Cucumber';
+
+use LWP::UserAgent;
+use HTTP::Request;
+use JSON::MaybeXS qw(encode_json decode_json);
+use JSON::Validator;
+
+use Test::More;
+use Test::BDD::Cucumber::StepFile;
+
+use Data::Dumper;
+
+use Kernel::System::ObjectManager;
+
+$Kernel::OM = Kernel::System::ObjectManager->new();
+
+# require our helper
+require '_Helper.pl';
+
+# require our common library
+require '_StepsLib.pl';
+
+# feature specific steps 
+
+When qr/I query the collection of session user counters$/, sub {
+   ( S->{Response}, S->{ResponseContent} ) = _Get(
+      Token => S->{Token},
+      URL   => S->{API_URL}.'/session/user/counters',
+   );
+};
