8820df484129309437f42763e72d1a78435aa11a
Mario Bauer <mario.bauer@kixdesk.com>
2025-12-05 12:14:12 +0100

    KIX2018-15230: fix report statement for mysql

--- update/build-2239.pl
+++ update/build-2239.pl
@@ -31,6 +31,9 @@ use vars qw(%INC);
 # migrate OutOfOffice prefs to users table
 _MigrateOOOPrefs();
 
+# update chart report definition for mysql
+_UpdateChartReports();
+
 sub _MigrateOOOPrefs {
     my ( $Self, %Param ) = @_;
 
@@ -62,6 +65,77 @@ sub _MigrateOOOPrefs {
     $DBObject->Prepare(
         SQL => "DELETE FROM user_preferences WHERE preferences_key IN ('OutOfOfficeStart', 'OutOfOfficeEnd', 'OutOfOfficeSubstitute')",
     );
+
+    return 1;
+}
+
+sub _UpdateChartReports {
+    my ( $Self, %Param ) = @_;
+
+    my @Definitions = (
+        {
+            Name      => Kernel::Language::Translatable('Number of tickets created within the last 7 days'),
+            OldConfig => {
+                'DataSource' => {
+                    'SQL' => {
+                        'postgresql' => 'base64(U0VMRUNUIGRhdGUoY3JlYXRlX3RpbWUpIGFzIGRheSwgQ291bnQoKikgYXMgY291bnQgRlJPTSB0aWNrZXQKV0hFUkUgKGRhdGUoY3JlYXRlX3RpbWUpIEJFVFdFRU4gZGF0ZSgoTk9XKCkgLSBJTlRFUlZBTCAnNyBEQVknKSkgQU5EIGRhdGUoTk9XKCkpKQpHUk9VUCBCWSBkYXkKb3JkZXIgYnkgZGF5IEFTQzs=)',
+                        'mysql'      => 'base64(U0VMRUNUIGRhdGUoY3JlYXRlX3RpbWUpIGFzIGRheSwgQ291bnQoKikgYXMgY291bnQgRlJPTSB0aWNrZXQKV0hFUkUgKGRhdGUoY3JlYXRlX3RpbWUpIEJFVFdFRU4gVElNRVNUQU1QKCBEQVRFX0ZPUk1BVChDVVJSRU5UX0RBVEUgLSBJTlRFUlZBTCA3IERBWSAsJyVZLSVtLTAxJykpIEFORCBjdXJyZW50X3RpbWUpCkdST1VQIEJZIGRheQpvcmRlciBieSBkYXkgQVNDOw==)'
+                    }
+                },
+                "OutputFormats" => {
+                    "CSV" => {
+                        "IncludeColumnHeader"   => 1,
+                        "Quote"                 => "\"",
+                        "Separator"             => ",",
+                        "TranslateColumnNames"  => 0
+                    }
+                }
+            },
+            NewConfig => {
+                'DataSource' => {
+                    'SQL' => {
+                        'postgresql' => 'base64(U0VMRUNUIGRhdGUoY3JlYXRlX3RpbWUpIGFzIGRheSwgQ291bnQoKikgYXMgY291bnQgRlJPTSB0aWNrZXQKV0hFUkUgKGRhdGUoY3JlYXRlX3RpbWUpIEJFVFdFRU4gZGF0ZSgoTk9XKCkgLSBJTlRFUlZBTCAnNyBEQVknKSkgQU5EIGRhdGUoTk9XKCkpKQpHUk9VUCBCWSBkYXkKb3JkZXIgYnkgZGF5IEFTQzs=)',
+                        'mysql'      => 'base64(U0VMRUNUIGRhdGUoY3JlYXRlX3RpbWUpIGFzIGRheSwgQ291bnQoKikgYXMgY291bnQgRlJPTSB0aWNrZXQKV0hFUkUgKGRhdGUoY3JlYXRlX3RpbWUpIEJFVFdFRU4gVElNRVNUQU1QKCBEQVRFX0ZPUk1BVChDVVJSRU5UX0RBVEUgLSBJTlRFUlZBTCA3IERBWSAsJyVZLSVtLSVkJykpIEFORCBjdXJyZW50X3RpbWUpCkdST1VQIEJZIGRheQpvcmRlciBieSBkYXkgQVNDOw==)'
+                    }
+                },
+                "OutputFormats" => {
+                    "CSV" => {
+                        "IncludeColumnHeader"   => 1,
+                        "Quote"                 => "\"",
+                        "Separator"             => ",",
+                        "TranslateColumnNames"  => 0
+                    }
+                }
+            }
+        }
+    );
+
+    for my $Definition ( @Definitions ) {
+        my $ReportDefinitionID = $Kernel::OM->Get('Reporting')->ReportDefinitionLookup(
+            Name => $Definition->{Name},
+        );
+
+        if ( $ReportDefinitionID ) {
+             my %ReportDefinitionData = $Kernel::OM->Get('Reporting')->ReportDefinitionGet(
+                ID => $ReportDefinitionID,
+            );
+
+            if (
+                !DataIsDifferent(
+                    Data1 => $ReportDefinitionData{Config},
+                    Data2 => $Definition->{OldConfig},
+                )
+            ) {
+
+                my $Success = $Kernel::OM->Get('Reporting')->ReportDefinitionUpdate(
+                    %ReportDefinitionData,
+                    ID     => $ReportDefinitionID,
+                    Config => $Definition->{NewConfig},
+                    UserID => 1,
+                );
+            }
+        }
+    }
 
     return 1;
 }
